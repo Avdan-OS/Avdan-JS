a76c4774ee2f7a4eb9bab93c3d845feb904228c6
Akane <96370213+akane6704@users.noreply.github.com>
2022-08-06 16:29:15 +0200

    Refactored utils.rs

--- src/Avdan/utils.rs
+++ src/Avdan/utils.rs
@@ -1,23 +1,25 @@
 use std::intrinsics::transmute;
-
 use v8::{FixedArray, Local, HandleScope, Data, Array, Value};
 
 pub fn fixed_array_to_vec<'a, R>(scope: &mut HandleScope<'a>, array: Local<FixedArray>) -> Vec<Local<'a, R>> {
     let mut out : Vec<Local<R>> = vec![];
+    
     for i in 0..array.length() {
-        out.push(
+        out.push (
             unsafe {
                 transmute::<Local<Data>, Local<R>>(array.get(scope, i).unwrap())
             }
         )
     }
+    
     out
 }
 
 pub fn array_to_vec<'a, R>(scope: &mut HandleScope<'a>, array: Local<Array>) -> Vec<Local<'a, R>> {
     let mut out : Vec<Local<R>> = vec![];
+    
     for i in 0..array.length() {
-        out.push(
+        out.push (
             unsafe {
                 transmute::<Local<Value>, Local<R>>(array.get_index(scope, i).unwrap())
             }

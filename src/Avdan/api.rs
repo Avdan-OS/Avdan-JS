81c5ab584f7c0c7254e6dc4f4d50bcab1a344b86
Akane <96370213+akane6704@users.noreply.github.com>
2022-08-06 16:18:41 +0200

    Refactored api.rs

--- src/Avdan/api.rs
+++ src/Avdan/api.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-
 use v8::{HandleScope, Local, Object,};
 
 // mod clipboard;
@@ -17,14 +16,13 @@ use self::{debug::AvDebug, net::AvNet};
 pub struct AvdanAPI {}
 
 impl AvdanAPI {
-
     fn assign_mod_to_obj<'a>(scope: &mut HandleScope<'a>, obj: Local<Object>, name: &str, module: &impl JSApi) -> () {
         let k = v8::String::new(scope, name).unwrap();
         let tmp = module.js(scope);
+        
         obj.define_own_property(scope, k.into(), tmp.into(), v8::READ_ONLY);
     }
     
-
     fn assign_values<'a>(scope: &mut HandleScope<'a>, obj : Local<Object>) -> () {
         // Self::assign_mod_to_obj(scope, obj, "Clipboard", &AvClipboard {});
         Self::assign_mod_to_obj(scope, obj, "Debug", &AvDebug {});
@@ -33,6 +31,7 @@ impl AvdanAPI {
 
     pub fn public_apis<'a>() -> HashMap<&'static str, Box<dyn JSApi>> {
         let mut h : HashMap<_, Box<dyn JSApi>> = HashMap::new();
+        
         // h.insert("clipboard", Box::new(AvClipboard{}));
         h.insert("debug", Box::new(AvDebug {}));
         h.insert("shell", Box::new(AvShell {}));
@@ -45,6 +44,7 @@ impl JSApi for AvdanAPI {
     fn js<'a>(&self, scope: &mut HandleScope<'a>) -> Local<'a, Object> {
         let obj = v8::Object::new(scope);
         Self::assign_values(scope, obj);
+        
         obj
     }
 }

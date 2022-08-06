df9ff852d65bf78a1f4937a1a8b0a58a4ba29cb2
Akane <96370213+akane6704@users.noreply.github.com>
2022-08-06 16:03:45 +0200

    Refactored js.rs

--- src/Avdan/runtime/avmod/js.rs
+++ src/Avdan/runtime/avmod/js.rs
@@ -10,14 +10,15 @@ use super::{AvModProvider, AvMod, Specifier};
 pub struct AvModJS {}
 
 impl AvModJS {
-    fn default_script_origin<'a, 'b>(
-        scope: &mut HandleScope<'a>,
-        name : &str,
-        source_map_url: &str, 
+    fn default_script_origin<'a, 'b> (
+        scope          : &mut HandleScope<'a>,
+        name           : &str,
+        source_map_url : &str, 
     ) -> ScriptOrigin<'a> {
         let name = v8::String::new(scope, name).unwrap();
         let source_map_url = v8::String::new(scope, source_map_url).unwrap();
-        ScriptOrigin::new(
+        
+        ScriptOrigin::new (
             scope,
             name.into(),
             0i32,
@@ -33,9 +34,9 @@ impl AvModJS {
 }
 
 impl AvModProvider for AvModJS {
-    fn load_module<'a>(
-        scope: &mut TryCatch<HandleScope<'a>>,
-        path : &PathBuf
+    fn load_module<'a> (
+        scope : &mut TryCatch<HandleScope<'a>>,
+        path  : &PathBuf
     ) -> Result<Global<Module>, String> {
         let source_text = v8::String::new(
             scope,
@@ -49,7 +50,8 @@ impl AvModProvider for AvModJS {
         );
 
         let source_code = Source::new(source_text, Some(&origin));
-        let module= match v8::script_compiler::compile_module(scope, source_code) {
+        
+        let module = match v8::script_compiler::compile_module(scope, source_code) {
             Some(s) => s,
             None => {
                 let err = scope.exception().unwrap();
@@ -62,9 +64,13 @@ impl AvModProvider for AvModJS {
             }
         };
 
-
         // println!("");
-        // println!("[{1}] {0}:", format!("Imports ({})", module.get_module_requests().length()).to_string().blue(), path.file_name().unwrap().to_str().unwrap().to_string().yellow());
+        // println! (
+        //     "[{1}] {0}:",
+        //     format!("Imports ({})",
+        //     module.get_module_requests().length()).to_string().blue(),
+        //     path.file_name().unwrap().to_str().unwrap().to_string().yellow()
+        // );
 
         for import in utils::fixed_array_to_vec::<ModuleRequest>(scope, module.get_module_requests()) {
             let name = import.get_specifier();
@@ -73,21 +79,20 @@ impl AvModProvider for AvModJS {
                 Ok(v) => v,
                 Err(err) => panic!("{}", err)
             };   
+            
             // println!("   {}\t{}", Colorize::bright_red("*").bold(), res);
 
             let dependency = AvMod::load(
                 scope,
                 &path.parent().unwrap().canonicalize().unwrap(), res.clone())?;
 
-
-            
             let store  =scope.get_slot_mut::<AvModStore>().unwrap();
             store.register(res, dependency);
         }
+        
         // println!("");
-
-
-        match module.instantiate_module(
+        
+        match module.instantiate_module (
             scope, 
             Self::_instantiate_callback
         ) {
@@ -99,30 +104,27 @@ impl AvModProvider for AvModJS {
             _ => {}
         };
 
-
         Ok(Global::new(scope, module))
     }
 
-    fn _instantiate_callback<'a>(
-        context: v8::Local<'a, v8::Context>,
-        specifier: v8::Local<'a, v8::String>,
-        import_assertions: v8::Local<'a, v8::FixedArray>,
-        dependent: v8::Local<'a, v8::Module>,
+    fn _instantiate_callback<'a> (
+        context           : v8::Local<'a, v8::Context>,
+        specifier         : v8::Local<'a, v8::String>,
+        import_assertions : v8::Local<'a, v8::FixedArray>,
+        dependent         : v8::Local<'a, v8::Module>,
     ) -> Option<v8::Local<'a, v8::Module>> {
         let scope = &mut unsafe {
             CallbackScope::new(context)
         };
 
         // println!("[{}] {}", specifier.to_rust_string_lossy(scope).yellow(), Colorize::blue("Callback from instantiation"), );
-        
         // println!("[{}] Import assertions: {}", specifier.to_rust_string_lossy(scope).yellow(), import_assertions.length());
         
         let res : Specifier = specifier.to_rust_string_lossy(scope).try_into().unwrap();
-        let store  =scope.get_slot_mut::<AvModStore>().unwrap();
+        let store = scope.get_slot_mut::<AvModStore>().unwrap();
         
-        let sender = store.get_sender(&dependent.script_id().unwrap());
+        let sender    = store.get_sender(&dependent.script_id().unwrap());
         let dependency = store.get(&res).expect("Could not find loaded module (!)");
-
 
         let s = &mut unsafe {
             CallbackScope::new(context)

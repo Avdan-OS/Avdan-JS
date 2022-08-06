88d0df2399495af17f952b98c8c401889dc26ec7
Akane <96370213+akane6704@users.noreply.github.com>
2022-08-06 16:12:47 +0200

    Refactored avmod.rs

--- src/Avdan/runtime/avmod.rs
+++ src/Avdan/runtime/avmod.rs
@@ -14,9 +14,7 @@ pub use js::AvModJS;
 pub use json::AvModJSON;
 pub use resource::{Specifier, SourceFile};
 
-pub struct AvMod {
-
-}
+pub struct AvMod {}
 
 impl AvMod {
     pub fn load_file(path: &PathBuf) -> String {
@@ -25,8 +23,9 @@ impl AvMod {
 
     pub fn load_from_file<'a>(scope: &mut HandleScope<'a>, dir: &PathBuf, file: SourceFile) -> Result<Global<Module>, String> {
         let p = file.to_path(dir.to_str().unwrap().to_string())?;
+        
         match file {
-            f if f.is_js_file() => AvModJS::load_module(&mut TryCatch::new(scope), &p),
+            f if f.is_js_file()          => AvModJS::load_module(&mut TryCatch::new(scope), &p),
             f if f.extension() == "json" => AvModJSON::load_module(&mut TryCatch::new(scope), &p),
             f => Err(format!("Unrecognised file format {}", f.extension()))
         }
@@ -34,28 +33,29 @@ impl AvMod {
 
     pub fn load<'a>(scope: &mut HandleScope<'a>, dir: &PathBuf, resource: Specifier) -> Result<Global<Module>, String> {
         match resource {
-            Specifier::File(f) => Self::load_from_file(scope, dir, f),
-            Specifier::Module(id) => todo!(),
+            Specifier::File(f)      => Self::load_from_file(scope, dir, f),
+            Specifier::Module(id)   => todo!(),
             Specifier::Internal(id) => AvModInternal::get_internal_module(scope, id)
         }
     }
 
-    fn instantiate_callback<'a>(
-        _context: v8::Local<'a, v8::Context>,
-        _specifier: v8::Local<'a, v8::String>,
-        _import_assertions: v8::Local<'a, v8::FixedArray>,
-        referrer: v8::Local<'a, v8::Module>,
+    fn instantiate_callback<'a> (
+        _context           : v8::Local<'a, v8::Context>,
+        _specifier         : v8::Local<'a, v8::String>,
+        _import_assertions : v8::Local<'a, v8::FixedArray>,
+        referrer           : v8::Local<'a, v8::Module>,
      ) -> Option<v8::Local<'a, v8::Module>> {
         Some(referrer)
     }
 
-    fn evaluation_steps<'a>(
-        ctx: Local<'a, Context>,
-        module: Local<'a, Module>
+    fn evaluation_steps<'a> (
+        ctx    : Local<'a, Context>,
+        module : Local<'a, Module>
     ) -> Option<Local<'a, Value>> {
         let scope = &mut unsafe {
             CallbackScope::new(ctx)
         };
+        
         let scope = &mut TryCatch::new(scope);
 
         let default_key = v8::String::new(scope, "default").unwrap();
@@ -63,7 +63,7 @@ impl AvMod {
             default_key.into(), default_key.into());
 
         match r {
-            None => {
+            None    => {
                 let excep = scope.exception().unwrap();
                 panic!("{}", format!("Error from JS:\n\t{}", excep.to_rust_string_lossy(scope).as_str().bright_red()));
             },
@@ -91,10 +91,10 @@ pub trait AvModProvider {
         path : &PathBuf
     ) -> Result<Global<Module>, String>;
 
-    fn _instantiate_callback<'a>(
-        context: v8::Local<'a, v8::Context>,
-        specifier: v8::Local<'a, v8::String>,
-        import_assertions: v8::Local<'a, v8::FixedArray>,
-        referrer: v8::Local<'a, v8::Module>,
+    fn _instantiate_callback<'a> (
+        context           : v8::Local<'a, v8::Context>,
+        specifier         : v8::Local<'a, v8::String>,
+        import_assertions : v8::Local<'a, v8::FixedArray>,
+        referrer          : v8::Local<'a, v8::Module>,
     ) -> Option<v8::Local<'a, v8::Module>>;
 }

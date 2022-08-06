1eff0403c0d2670fa76829be28c6a1a6bfb4f93b
Akane <96370213+akane6704@users.noreply.github.com>
2022-08-06 16:32:22 +0200

    Refactored Avdan.rs

--- src/Avdan.rs
+++ src/Avdan.rs
@@ -27,30 +27,33 @@
 
 pub mod security;
 pub mod loader;
-mod runtime;
 pub mod api;
-
 pub mod utils;
 
+mod runtime;
+
 pub struct Permission {
-    contents: String,
+    contents : String,
 }
 
 impl Permission {
     pub fn new(contents : &str) -> Permission {
-        Permission { contents : String::from(contents) }
+        Permission {
+            contents : String::from(contents)
+        }
     }
+    
     pub fn has(&self, permission: &str) -> bool {
-        let to_check_arr : Vec<&str> = self.contents.split(".").collect();
+        let to_check_arr   : Vec<&str> = self.contents.split(".").collect();
         let permission_arr : Vec<&str> = permission.split(".").collect();
 
         for (i, el) in to_check_arr.iter().enumerate() {
             match *el {
                 "*" => return true,
-                n => {
+                n   => {
                     match permission_arr.get(i) {
                         Some(v) => if *v != n { return false; },
-                        None => return false,
+                        None    => return false,
                     }
                 } 
             }

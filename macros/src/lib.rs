e86926b35eb5050e0fc0183aada5748f97288c8e
Akane <96370213+akane6704@users.noreply.github.com>
2022-08-10 12:00:32 +0200

    Refactored lib.rs

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -4,12 +4,14 @@ use quote::{quote, ToTokens, format_ident};
 #[proc_macro_attribute]
 pub fn permission(attr: TokenStream, content : TokenStream) -> TokenStream {
     let mut item: syn::Item = syn::parse(content).unwrap();
+    
     let fn_item = match &mut item {
         syn::Item::Fn(fn_item) => fn_item,
         _ => panic!("expected fn !")
     };
 
     let mut scope_arg : Option<String> = None;
+    
     for arg in fn_item.sig.inputs.clone() {
         let tokens : Vec<String> = arg.into_token_stream().into_iter().map(|t| {
             t.to_string().clone()
@@ -23,7 +25,7 @@ pub fn permission(attr: TokenStream, content : TokenStream) -> TokenStream {
 
     let scope = match scope_arg {
         Some(a) => a,
-        None => panic!("fn must have &mut v8::HandleScope !")
+        None    => panic!("fn must have &mut v8::HandleScope!")
     };
 
     let perm = attr.to_string();
@@ -31,22 +33,27 @@ pub fn permission(attr: TokenStream, content : TokenStream) -> TokenStream {
     // Add these lines to the start of the function
     let scope_var = format_ident!("{}", scope);
 
-    let q = quote!{
-        match Avdan::security::Constraints::from_scope(#scope_var).throw_permission_exception(#scope_var, #perm) { true => {}, false => return};
+    let q = quote! {
+        match Avdan::security::Constraints::from_scope(#scope_var)
+              .throw_permission_exception(#scope_var, #perm) {
+          true  => {},
+          false => return
+        };
     };
 
-    let to_add = syn::parse(
+    let to_add = syn::parse (
         q.into()
     );
 
     match to_add {
-        Err(e) =>{
+        Err(e) => {
             panic!("{}",
-                format!(
+                format! (
                     "{:?}", e.into_compile_error()
                 )
             );
         },
+        
         Ok(body) => {
             fn_item.block.stmts.insert(0, body);
             fn_item.to_token_stream().into()
